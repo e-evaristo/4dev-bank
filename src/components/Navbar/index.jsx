56d51b422b50e7a17438e46311ad70fafe700773
e-evaristo <emanoelevaristo@hotmail.com>
2022-05-26 11:27:00 -0300

    #FIX Responsive Menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { RiMenuFill } from 'react-icons/ri';
 import { AuthContext } from "../../contexts/auth";
 
 import logo from '../../img/logo-wide.png';
@@ -8,22 +9,30 @@ import './navbar.css';
 
 const Navbar = () => {
     const { logout } = useContext(AuthContext);
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/home" className="logo"><img src={logo} alt="4DevBank" /></Link>
-                <ul className="list">
-                    <li className="item"><Link to="/home">Home</Link></li>
-                    <li className="item"><Link to="/accounts">Contas</Link></li>
-                    <li className="item"><Link to="/balances">Saldo</Link></li>
-                    <li className="item"><Link to="/extracts">Extrato</Link></li>
-                    <li className="item"><Link to="/operations">Operações</Link></li>
-                    <li className="item">
-                        <button className="logout-btn" onClick={ () => logout() } >
-                            Sair
-                        </button>
-                    </li>
-                </ul>
+                <div className="logo-top-bar">
+                    <Link to="/home" className="logo"><img src={logo} alt="4DevBank" /></Link>
+                    <RiMenuFill color="#FFF" size={28} onClick={() => setMenuOpen(!menuOpen)} />
+                </div>
+                
+                    <ul className={ menuOpen ? 'list':'list-open' }>
+                        <li className="item" onClick={() => setMenuOpen(true)}><Link to="/home">Home</Link></li>
+                        <li className="item" onClick={() => setMenuOpen(true)}><Link to="/accounts">Contas</Link></li>
+                        <li className="item" onClick={() => setMenuOpen(true)}><Link to="/balances">Saldo</Link></li>
+                        <li className="item" onClick={() => setMenuOpen(true)}><Link to="/extracts">Extrato</Link></li>
+                        <li className="item" onClick={() => setMenuOpen(true)}><Link to="/operations">Operações</Link></li>
+                        <li className="item">
+                            <button className="logout-btn" onClick={ () => logout() } >
+                                Sair
+                            </button>
+                        </li>
+                    </ul>
+                
             </div>
             
         </nav>
